d998a0c9938459d827ee10d08fcc5db7f44fb415
ss <starshistrelok@gmail.com>
2020-06-23 20:10:29 +0300

    Initial commit.

---
+++ src/main/java/org/ss/salesforce/tools/SecondaryController.java
@@ -0,0 +1,12 @@
+package org.ss.salesforce.tools;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
